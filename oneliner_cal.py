a6945fe6e99dfd2ca73f02f8d901308b4ad95a6a
Anshuman Gogoi <116825724+cyberhypex@users.noreply.github.com>
2023-07-19 10:03:05 +0530

    Update oneliner_cal.py

--- oneliner_cal.py
+++ oneliner_cal.py
@@ -2,5 +2,5 @@ print("Welcome to the one liner calculator \n")
 print("Enter your expression \n")
 
 x=input()
-print("Your expression gives the following result", eval(x))
+print("Your expression gives the following result \n", eval(x))
 print("Thank you for using this calculator \n Hope this helped you!!")
