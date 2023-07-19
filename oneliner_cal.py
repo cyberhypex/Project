e567e38835eb66195498234eb56390faa41ca6f1
Anshuman Gogoi <116825724+cyberhypex@users.noreply.github.com>
2023-07-19 10:00:26 +0530

    Add files via upload
    This is a mini project in Python wherein I have created a calculator where user can directly enter any expression for evaluation and get the desired result .I have  used the eval() function for this whole mini project.

---
+++ oneliner_cal.py
@@ -0,0 +1,6 @@
+print("Welcome to the one liner calculator \n")
+print("Enter your expression \n")
+
+x=input()
+print("Your expression gives the following result", eval(x))
+print("Thank you for using this calculator \n 88Hope this helped you!!")
